d921ea8f239933ac92dfd9765b0ab5831bb42c33
KwangJe Moon <iqboy963295@gmail.com>
2022-01-18 10:15:48 +0900

    인기영화페이지 작업

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Seo from "../components/Seo";
 import { useRouter } from "next/router";
+import Link from "next/link";
 export default function Home({ results }) {
   const router = useRouter();
   return (
@@ -8,6 +9,13 @@ export default function Home({ results }) {
       {results?.map((movie) => (
         <div key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href='#'>
+              <a>{movie.title}</a>
+            </Link>
+          </h4>
+          <div>평점 : {movie.vote_average}</div>
+          <div>개봉일자 : {movie.release_date}</div>
         </div>
       ))}
     </div>
