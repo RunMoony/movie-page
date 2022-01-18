af227f93f16011440d0ed764fabb4ad6d43b93a6
KwangJe Moon <iqboy963295@gmail.com>
2022-01-18 13:35:15 +0900

    인기영화 스타일 작업

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,15 +7,51 @@ export default function NavBar() {
     <nav>
       <div>
         <Link href='/'>
-          <a>인기영화</a>
+          <a className={router.pathname === "/" ? "active" : ""}>인기영화</a>
         </Link>
         <Link href='/now'>
-          <a>현재 상영영화</a>
+          <a className={router.pathname === "/now" ? "active" : ""}>
+            현재 상영영화
+          </a>
         </Link>
         <Link href='/expected'>
-          <a>개봉 예정영화</a>
+          <a className={router.pathname === "/expected" ? "active" : ""}>
+            개봉 예정영화
+          </a>
         </Link>
       </div>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+          color: rgba(255, 255, 255, 0.7);
+          margin-left: 10px;
+          margin-right: 10px;
+        }
+        a:hover {
+          color: white;
+        }
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: white;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,43 @@ import Link from "next/link";
 export default function Home({ results }) {
   const router = useRouter();
   return (
-    <div>
+    <div className='container'>
       <Seo title='Home' />
       {results?.map((movie) => (
-        <div key={movie.id}>
+        <div className='movie' key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>
-            <Link href='#'>
-              <a>{movie.title}</a>
-            </Link>
-          </h4>
-          <div>평점 : {movie.vote_average}</div>
-          <div>개봉일자 : {movie.release_date}</div>
+          <h4>평점 : {movie.vote_average}</h4>
+          <h4>개봉일자 : {movie.release_date}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie:hover h4 {
+          color: white;
+        }
+        .movie h4 {
+          color: rgba(255, 255, 255, 0.7);
+          font-size: 15px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
