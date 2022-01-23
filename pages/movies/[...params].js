d53c4d8267788820a1246acd61fb8c7114c19e6e
KwangJe Moon <iqboy963295@gmail.com>
2022-01-23 21:37:06 +0900

    등장인물 작업

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,6 +1,6 @@
 import Seo from "../../components/Seo";
 
-export default function Detail({ params, details }) {
+export default function Detail({ params, details, actors }) {
   const [title] = params || [];
   return (
     <div>
@@ -28,10 +28,30 @@ export default function Detail({ params, details }) {
             ? details.production_countries
                 .map((country) => country.name)
                 .join(" / ")
-            : "작성된 제조국이 없습니다."}
+            : "정보없음"}
         </p>
         <div className='credit'>
           <h3>출연배우</h3>
+          <ul>
+            {actors.cast.map((actor, idx) => (
+              <li key={idx}>
+                <img
+                  className='profile'
+                  src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                />
+                <h6 className='character'>
+                  역할명
+                  <br />
+                  {actor.character ? actor.character : "-"}
+                </h6>
+                <h6 className='name'>
+                  배우명
+                  <br />
+                  {actor.name ? actor.name : "정보없음"}
+                </h6>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <style jsx>{`
@@ -57,12 +77,35 @@ export default function Detail({ params, details }) {
           margin: 0px 0px 8px;
           font-weight: normal;
         }
+        h6 {
+          color: white;
+          font-size: 14px;
+        }
         p {
           color: white;
-          line-height: 30px;
+          font-size: 20px;
+          line-height: 35px;
+        }
+        ul {
+          white-space: nowrap;
+          overflow-x: auto;
+          margin-left: -40px;
+        }
+        li {
+          display: inline-block;
+          margin-right: 30px;
+        }
+        .name {
+          margin-top: -15px;
         }
         .credit {
           margin-top: 50px;
+        }
+        .profile {
+          max-width: 150px;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
       `}</style>
     </div>
@@ -76,7 +119,6 @@ export async function getServerSideProps({ params: { params } }) {
   const actors = await (
     await fetch(`http://localhost:3000/api/movies/${params[1]}/credits`)
   ).json();
-  console.log(actors.cast);
   return {
     props: {
       params,
